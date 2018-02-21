aebb9905229058a26737b679e4af33bcf03ed0ce
Anton Hansson <hansson@google.com>
2018-02-21 14:11:44 +0000

    LineageActions: Set LOCAL_PRIVATE_PLATFORM_APIS
    This change sets LOCAL_SDK_VERSION for all packages where
    this is possible without breaking the build, and
    LOCAL_PRIVATE_PLATFORM_APIS := true otherwise.
    
    Setting one of these two will be made required soon, and this
    is a change in preparation for that. Not setting LOCAL_SDK_VERSION
    makes the app implicitly depend on the bootclasspath, which is
    often not required. This change effectively makes depending on
    private apis opt-in rather than opt-out.
    
    Bug: 73535841
    Change-Id: I61299717ab2a6a1fd9a14dde7e1c7961388f722a
    Signed-off-by: Rohit Vashisht <rohitv88cf@gmail.com>

--- LineageActions/Android.mk
+++ LineageActions/Android.mk
@@ -9,7 +9,7 @@ LOCAL_PACKAGE_NAME := LineageActions
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_USE_AAPT2 := true
-LOCAL_SDK_VERSION := current
+LOCAL_PRIVATE_PLATFORM_APIS := true
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-support-v7-appcompat \
