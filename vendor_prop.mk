f0305b3bb8daddd1a1d8784092fb01ea4fe84961
Nich <nctrenco@gmail.com>
2018-09-13 01:02:11 +0800

    athene: Set virtual displays to 0
    We don't have any virtual displays, however HWC assumes 1 virtual
    display by default. This was causing laggy display when allocating
    virtual displays, as done by surfaceflinger during screen recording
    for example.
    
    Change-Id: Iab7ddb4182ee1d952f3083942eece033f651af98
    Signed-off-by: Rohit Vashisht <rohitv88cf@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -102,7 +102,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	debug.enable.sglscale=1 \
 	ro.opengles.version=196610 \
 	ro.sf.lcd_density=400 \
-	debug.hwui.use_buffer_age=false
+	debug.hwui.use_buffer_age=false \
+	debug.sdm.support_writeback=0
 
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
